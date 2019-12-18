792cef7685f00ae818187283cf706bfecb64cf96
Hallvalor <sarahsturmvii@gmx.de>
2019-12-18 17:03:48 +0100

    Update 18.12.

---
+++ Assets/TheGame/Scripts/Utils/SpriteAnimator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{   //Animates sprites
+    
+    public Sprite[] frames = new Sprite[0]; //list of sprites for destruction-animation
+    public float duration = 0.5f;
+    public bool loop = true;
+    public bool destroyObject = false;
+
+    public void OnEnable()
+    {
+        StartCoroutine(playAni());
+    }
+
+
+    protected IEnumerator playAni()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        do  //while the object is enabled (is false if object is deleted) && loop = true => animation forever
+        {
+            for (int i = 0; i < frames.Length; i++)
+            {
+                if (!enabled) break;
+                sr.sprite = frames[i];
+                yield return new WaitForSeconds(duration / frames.Length); //wait for xy sec (1 = 1 sec) //Game continues
+            }
+        }
+        while (enabled && loop);
+
+        if (destroyObject)
+            Destroy(gameObject);
+    }
+
+}
